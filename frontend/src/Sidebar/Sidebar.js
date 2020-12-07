7bfee9e7f301e10778084ed0fab2e81e7ab0f09c
Johan Andersson <ja222um@student.lnu.se>
2020-12-07 22:40:07 +0100

    Implemented menu items with icons.

--- frontend/src/Sidebar/Sidebar.js
+++ frontend/src/Sidebar/Sidebar.js
@@ -1,18 +1,52 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 
 import './Sidebar.css'
 
+import MenuLink from './components/MenuLink'
+
 const generalNavLinks = [
   {
-    name: 'dashboard',
+    name: 'Dashboard',
     url: '/'
   },
   {
-    name: 'roster',
+    name: 'Roster',
     url: '/roster'
+  },
+  {
+    name: 'Help',
+    url: '/help'
+  },
+  {
+    name: 'Settings',
+    url: '/settings'
   }
+]
 
+const spreadSheetLinks = [
+  {
+    name: 'Link',
+    url: '/spreadsheet'
+  },
+  {
+    name: 'Refresh',
+    url: '/spreadsheet/status'
+  }
+]
+
+const raidLinks = [
+  {
+    name: 'Calendar',
+    url: '/raids/calendar'
+  },
+  {
+    name: 'Statistics',
+    url: '/raids/statistics'
+  },
+  {
+    name: 'Wishlist',
+    url: '/raids/wishlist'
+  }
 ]
 
 const Sidebar = () =>
@@ -20,7 +54,15 @@ const Sidebar = () =>
     <ul>
       <li className='header'>General</li>
       {generalNavLinks.map(item =>
-        <li><NavLink to={item.url} className='MenuLink'>{item.name}</NavLink></li>
+        <MenuLink text={item.name} url={item.url} />
+      )}
+      <li className='header'>Spreadsheet</li>
+      {spreadSheetLinks.map(item => 
+        <MenuLink text={item.name} url={item.url} />
+      )}
+      <li className='header'>Raids</li>
+      {raidLinks.map(item => 
+        <MenuLink text={item.name} url={item.url} />
       )}
     </ul>
   </nav>
---
+++ frontend/src/Sidebar/components/MenuLink/MenuLink.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as icon from '@fortawesome/free-solid-svg-icons'
+
+import './MenuLink.css'
+
+const getIcon = (name) => {
+  switch(name) {
+    case 'Dashboard':
+      return icon.faAnchor
+    case 'Roster':
+      return icon.faUserFriends
+    case 'Help':
+      return icon.faQuestion
+    case 'Settings':
+      return icon.faCogs
+    case 'Link':
+      return icon.faLink
+    case 'Refresh':
+      return icon.faSync
+    case 'Calendar':
+      return icon.faCalendarDay
+    case 'Statistics':
+      return icon.faChartBar
+    case 'Wishlist':
+      return icon.faClipboardList
+    default:
+      return icon.faBox
+  }
+}
+
+const MenuLink = ({text, url}) =>
+  <li className='link'>
+    <NavLink exact to={url} className='MenuLink' activeClassName='ActiveLink'>
+      <FontAwesomeIcon icon={getIcon(text)} className='MenuIcon'/> {text}
+    </NavLink>
+  </li>
+
+export default MenuLink
