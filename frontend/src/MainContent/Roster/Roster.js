dfc7006d9110d784c4e947c156474474c75ed1b3
Johan Andersson <ja222um@student.lnu.se>
2020-12-18 17:27:45 +0100

    Created own useSortable hook. Now possible to sort roster table both asc and desc on a number of values.

--- frontend/src/MainContent/Roster/Roster.js
+++ frontend/src/MainContent/Roster/Roster.js
@@ -5,10 +5,42 @@ import * as icon from '@fortawesome/free-solid-svg-icons'
 
 import './Roster.css'
 import RosterService from 'services/RosterService'
+import useSortableData from 'hooks/useSortableData'
 
 const Roster = () => {
   const [roster, setRoster] = useState([])
+  //const [sortConfig, setSortConfig] = useState(null)
   const [test, setTest] = useState('')
+
+  const { items, requestSort, sortConfig } = useSortableData(roster)
+
+  const getClassNamesFor = name => {
+    if (!sortConfig) {
+      return
+    }
+    return sortConfig.key === name ? sortConfig.direction: undefined
+  }
+
+/*
+  if (sortConfig) {
+    roster.sort((a,b ) => {
+      if (a[sortConfig.key] < b[sortConfig.key]) {
+        return sortConfig.direction === 'ascending' ? -1 : 1
+      }
+      if (a[sortConfig.key] > b[sortConfig.key]) {
+        return sortConfig.direction === 'ascending' ? 1 : -1
+      }
+      return 0
+    })
+  }
+
+  const requestSort = key => {
+    let direction = 'ascending'
+    if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending'
+    }
+    setSortConfig({key, direction})
+  }*/
 
   useEffect(() => {
     retrieveRoster()
@@ -19,6 +51,11 @@ const Roster = () => {
       const response = await RosterService.getAll()
       const rost = await response.data
       console.log('rost!', rost)
+      const sorted = rost.sort((a, b) => { 
+        if (a.name < b.name) { return -1 }
+        if (a.name > b.name) { return 1 }
+       })
+      console.log('sort', sorted)
       setRoster(rost)
       setTest('test')
       console.log('set?', roster)
@@ -35,7 +72,7 @@ const Roster = () => {
         <thead>
           <tr>
             <th className='RoName'>
-              <button onClick={() => setRoster('name')}>
+              <button onClick={() => requestSort('name')} className={getClassNamesFor('name')}>
                 Name
               </button>
             </th>
@@ -43,34 +80,34 @@ const Roster = () => {
               Realm
             </th>
             <th className='RoRole'>
-              <button onClick={() => setRoster('role')}>
+              <button onClick={() => requestSort('role')} className={getClassNamesFor('role')}>
                 Role
               </button>
             </th>
             <th className='RoRank'>
-              <button onClick={() => setRoster('rank')}>
+              <button onClick={() => requestSort('rank')} className={getClassNamesFor('rank')}>
                 Rank
               </button>
             </th>
             <th className='RoLvl'>
-              <button onClick={() => setRoster('lvl')}>
+              <button onClick={() => requestSort('level')} className={getClassNamesFor('level')}>
                 Level
               </button>
             </th>
             <th>
-              <button onClick={() => setRoster('log')}>
+              <button onClick={() => requestSort('lastLogin')} className={getClassNamesFor('lastLogin')}>
                 Last login
               </button>
             </th>
             <th className='RoUpdated'>
-              <button onClick={() => setRoster('upd')}>
+              <button onClick={() => requestSort('updatedAt')} className={getClassNamesFor('updatedAt')}>
                 Updated
               </button>
             </th>
           </tr>
         </thead>
         <tbody>
-          {roster.filter(character => (character.rank === 1 || character.rank === 2 || character.rank === 4)).map(character => (
+          {roster.filter(character => (character.rank === 0 || character.rank === 1 || character.rank === 2 || character.rank === 4)).map(character => (
             <tr key={character.id}>
               <td>
                 <img src={character.activeSpecIcon} className='PlayerSpecIcon'/>
